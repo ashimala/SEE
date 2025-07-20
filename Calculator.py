7c0bcbc16aeaf255b3b00805e69ab30c31152f62
ashimala <ashimalacasper@gmail.com>
2025-07-20 12:36:56 +0300

    Added My calculator file

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+num1=int(input("Input the first number: "))
+num2=int(input("Input the second number: "))
+#addition
+addition= num1+num2
+
+#product
+product=num1*num2
+
+#quotient
+Quotient=num1/num2
+
+#Subtration
+
+Subtraction=num1-num2
+
+#Output
+print(f'Your addition of the two numbers is:{addition}')
+
+print(f'Your product of the two numbers is:{product}')
+
+print(f'Your quotient of the two numbers is:{Quotient}')
+
+print(f'Your subtraction of the two numbers is:{Subtraction}')
